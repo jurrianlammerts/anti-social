d96b048af2cb6640c87857534d673da3f08fece0
Jurrian <jurrianlammerts@hotmail.com>
2020-05-16 12:38:03 +0200

    small fix will hopefully work

--- src/useIntersect.js
+++ src/useIntersect.js
@@ -3,14 +3,22 @@ import { useEffect, useRef, useState } from "react"
 export default ({ root = null, rootMargin, threshold = 0 }) => {
   const [entry, updateEntry] = useState({})
   const [node, setNode] = useState(null)
+  const [loaded, setLoaded] = useState(false)
 
-  const observer = useRef(
-    new window.IntersectionObserver(([entry]) => updateEntry(entry), {
-      root,
-      rootMargin,
-      threshold,
-    })
-  )
+  // This value is set to true when this code runs in the browser
+  useEffect(() => {
+    setLoaded(true)
+  }, [])
+
+  if (loaded) {
+    const observer = useRef(
+      new window.IntersectionObserver(([entry]) => updateEntry(entry), {
+        root,
+        rootMargin,
+        threshold,
+      })
+    )
+  }
 
   useEffect(() => {
     const { current: currentObserver } = observer
