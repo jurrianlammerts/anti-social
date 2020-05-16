24c2ebd2d181e1e66173c8d380600ac237b92360
Jurrian <jurrianlammerts@hotmail.com>
2020-05-16 14:16:24 +0200

    pwa performance test

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,7 +32,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={title}
+      title="Anti Social"
       meta={[
         {
           name: `description`,
