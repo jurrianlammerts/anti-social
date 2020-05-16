f4d478a317dab37955481eeb743c4271515ca869
Jurrian <jurrianlammerts@hotmail.com>
2020-05-16 13:46:46 +0200

    Added some sick content + minimal logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Github from "./icons/github"
+import Logo from "./icons/logo"
 
 const Header = ({ siteTitle }) => {
   return (
     <header>
-      <a
-        href="https://github.com/jurrianlammerts"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="logo"
-      >
-        <Github />
-      </a>
+      <div className="logo">
+        <a href="/">
+          <Logo />
+        </a>
+      </div>
+      <div className="logo">
+        <a
+          href="https://github.com/jurrianlammerts"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+        </a>
+      </div>
     </header>
   )
 }
--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,12 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+      />
+    </Helmet>
   )
 }
 
