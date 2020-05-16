b958bcf8a7436190019a8345719c6313384c457c
Jurrian <jurrianlammerts@hotmail.com>
2020-05-16 12:45:35 +0200

    Check for window var

--- src/useIntersect.js
+++ src/useIntersect.js
@@ -3,22 +3,17 @@ import { useEffect, useRef, useState } from "react"
 export default ({ root = null, rootMargin, threshold = 0 }) => {
   const [entry, updateEntry] = useState({})
   const [node, setNode] = useState(null)
-  const [loaded, setLoaded] = useState(false)
 
   // This value is set to true when this code runs in the browser
-  useEffect(() => {
-    setLoaded(true)
-  }, [])
 
-  if (loaded) {
-    const observer = useRef(
+  const observer = useRef(
+    window &&
       new window.IntersectionObserver(([entry]) => updateEntry(entry), {
         root,
         rootMargin,
         threshold,
       })
-    )
-  }
+  )
 
   useEffect(() => {
     const { current: currentObserver } = observer
